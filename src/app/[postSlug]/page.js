aa09e640b3a1384876d14d13343060ef4520c532
Birdie <7945905+Birdie0@users.noreply.github.com>
2024-04-17 14:13:26 +0300

    exercise 2 + 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,24 +3,37 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import { loadBlogPost } from '@/helpers/file-helpers';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { BLOG_TITLE } from '@/constants';
 
-function BlogPost() {
+export async function getData(postSlug) {
+  return loadBlogPost(postSlug)
+}
+
+async function BlogPost({ params: { postSlug } }) {
+  const { content, frontmatter } = await getData(postSlug)
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content}/>
       </div>
     </article>
   );
+}
+
+export async function generateMetadata({ params: { postSlug } }) {
+  const { frontmatter: { title, abstract } } = await getData(postSlug)
+
+  return {
+    title: `${BLOG_TITLE} - ${title}`,
+    description: abstract,
+  }
 }
 
 export default BlogPost;
